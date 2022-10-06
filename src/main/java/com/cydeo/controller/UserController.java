29db8a7a223dc78382b5fea9d8745106e908c856
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-06 17:48:34 +0200

    ticketing project practice

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.dto.UserDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/create")
-    public String getUserCreatePage(){
-
-        //task
+    @GetMapping("/create") // UserCreate Page
+    public String getUserCreatePage(Model model){
+        //user
+        model.addAttribute("user",new UserDTO());
         //role
+        //model.addAttribute("roles", .....);
         //userList
+        //model.addAttribute("users", ....);
 
         return "/user/create";
     }
